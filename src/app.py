16055e2fdb19eb65f0706f7e5137910871d1692b
Jessica Leishman <71527196+jesscleish@users.noreply.github.com>
2023-11-16 21:38:38 -0500

    uploading cert needed for connection and commented code

--- src/app.py
+++ src/app.py
@@ -21,14 +21,6 @@ db = MySQLdb.connect(host=os.getenv('DB_HOST'),    # your host, usually localhos
                      ssl={
                          "ca": "./cacert.pem"
                      })
-
-#defining class for part number object
-class PartNum:
-    def __init__(self, pn, wh, qty):
-        self.pn = pn
-        self.wh = wh
-        self.qty = qty
-
 @app.route('/', methods=['GET'])
 def greet():
     return "Welcome to Jessica Leishman's (100747155) Manufacturing/Inventory Management API"
@@ -37,6 +29,7 @@ def greet():
 def welcome():
     return "Hello World!"
 
+#Retrieves all parts
 @app.get('/getAll')
 def get_all_parts():
     #does not use warehouse as a filter, assumes user wants to know all locations
@@ -56,8 +49,8 @@ def get_all_parts():
     else:
         return jsonify({'result': 'ERROR', 'msg': 'could not retrieve parts'}), 400
 
-#TODO
-# if part doesnt exist, return error
+
+#get inventory for a part number in all warehouses
 @app.get('/getPN/<string:id>')
 def get_pn_inventory(id):
     #does not use warehouse as a filter, assumes user wants to know all locations
@@ -69,33 +62,35 @@ def get_pn_inventory(id):
     if rows > 0:
         qty = c.fetchone()
         # Return the results as JSON
-        return jsonify({'result':'OK -- RETRIEVED', 'part': pn, 'quantity': qty[0]}), 200
+        if qty[0] != None:
+            return jsonify({'result':'OK -- RETRIEVED', 'part': pn, 'quantity': qty[0]}), 200
+        else:
+            return jsonify({'result': 'ERROR', 'msg': 'part does not exist!', 'part': id}), 200
     else:
-        return jsonify({'result': 'ERROR', 'msg': 'part does not exist!', 'part':id}), 400
+        return jsonify({'result': 'ERROR', 'msg': 'could not query db'}), 400
 
-#TODO
-# if part+wh doesnt exist, return error
+#get inventory for a part number in a specific warehouse
 @app.get('/getPN/<string:id>/<int:wh>')
 def get_pn_wh_inventory(id,wh):
     #does use warehouse as a filter,
-    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND  warehouse_id=%d"""
+    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND  warehouse_id=%s"""
     c = db.cursor()
     rows = c.execute(query, [id,wh])
     if rows > 0:
         qty = c.fetchone()
-
-        # Return the results as JSON
-        return jsonify({'result':'OK -- RETRIEVED','part': id, 'warehouse': wh, 'quantity': qty[0]}), 200
+        if qty[0] != None:
+            # Return the results as JSON
+            return jsonify({'result':'OK -- RETRIEVED','part': id, 'warehouse': wh, 'quantity': qty[0]}), 200
+        else:
+            return jsonify({'result': 'ERROR', 'msg': 'part and wh combo does not exist!', 'part': id, 'wh': wh}), 200
     else:
-        return jsonify({'result': 'ERROR', 'msg': 'part and wh combo does not exist!', 'part': id, 'wh': wh}), 400
+        return jsonify({'result': 'ERROR', 'msg': 'could not query db'}), 400
 
 
-#TODO
-# if pn doesnt exist, need to return error msg
-# pick supplierID for that pn with highest priority to subtract from.  if highest priority doesnt have inventory, go to second
+#Place an order (would typically trigger a request for internal systems)
 @app.post('/order/<string:id>/<int:wh>')
 def order_part(id, wh):
-    query = """SELECT quantity, supplier_id FROM PARTS WHERE part_number =%s AND  warehouse_id=%d ORDER BY priority ASC"""
+    query = """SELECT quantity, supplier_id FROM PARTS WHERE part_number =%s AND  warehouse_id=%s ORDER BY priority ASC"""
     c = db.cursor()
     rows = c.execute(query, [id, wh])
     if rows > 0:
@@ -106,13 +101,16 @@ def order_part(id, wh):
         except:
             return jsonify({'result': 'ERROR', 'msg': 'quantity 0 for all suppliers', 'part': id, 'warehouse': wh}), 400
 
+        if qty[0] == None:
+            return jsonify({'result': 'ERROR', 'msg': 'quantity 0 for all suppliers', 'part': id, 'warehouse': wh}), 400
+
         #if there is a valid quantity, need to update it so does not get over ordered from
-        qty[0] = qty[0]-1
-        query = """UPDATE TABLE PARTSS SET quantity =%d WHERE part_number =%s AND warehouse_id=%d AND supplier_id=%s ORDER BY priority ASC"""
-        rows2 = c.execute(query, [qty[0], id, wh, qty[1]])
+        newqty = int(qty[0])-1
+        query = """UPDATE PARTS SET quantity =%s WHERE part_number =%s AND warehouse_id=%s AND supplier_id=%s ORDER BY priority ASC"""
+        rows2 = c.execute(query, [newqty, id, wh, str(qty[1])])
         if rows2 > 0:
             # Return the results as JSON
-            return jsonify({'result': 'OK -- ORDERED', 'part': id, 'wh': wh, 'supplier': qty[1], 'remaining quantity': qty[0]}), 200
+            return jsonify({'result': 'OK -- ORDERED', 'part': id, 'wh': wh, 'supplier': qty[1], 'remaining quantity': newqty}), 200
         else:
             return jsonify({'result': 'ERROR', 'msg': 'couldnt update quantity to place order', 'part': id, 'wh': wh, 'supplier': qty[1]}), 400
     else:
@@ -120,11 +118,11 @@ def order_part(id, wh):
                         'warehouse': wh}), 400
     return jsonify({'result': 'OK', 'pn': id})
 
-#TODO
-# fix error msg response, include HTTP status
+
+#Add a part number, wh and supplier id to the database
 @app.put('/addPN/<string:id>/<int:wh>/<string:suppID>')
 def add_pn(id, wh, suppID):
-    query = """INSERT INTO PARTS VALUES (%s, %s, %d, %d)"""
+    query = """INSERT INTO PARTS VALUES (%s, %s, %s, %s)"""
     c = db.cursor()
     rows = c.execute(query, [id, suppID, wh, 0])
 
@@ -134,38 +132,37 @@ def add_pn(id, wh, suppID):
         return jsonify({'result': 'ERROR', 'msg': 'Could not add value combination!', 'pn': id, 'warehouse': wh, 'supplier': suppID, 'qty': 0})
 
 
-#TODO
-# fix error msg response, include HTTP status
+#Increments the quantity fora given pn/wh/suppID combination
 @app.put('/addQty/<string:id>/<int:wh>/<string:suppID>/<int:qty>')
 def add_qty(id,wh,suppID,qty):
-    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND warehouse_id=%d AND supplier_id=%s"""
+    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND warehouse_id=%s AND supplier_id=%s"""
     c = db.cursor()
     rows = c.execute(query, [id, wh,suppID])
 
     if rows > 0:
         currentQty = c.fetchone()
-        currentQty = currentQty + qty
+        updatedVal = int(currentQty[0]) + int(qty)
 
-        query = """UPDATE TABLE PARTS SET quantity=%d WHERE part_number=%s AND warehouse_id=%d AND supplier_id=%s"""
-        rowsImpact = c.execute(query, [currentQty, id, wh, suppID])
+        query = """UPDATE PARTS SET quantity=%s WHERE part_number=%s AND warehouse_id=%s AND supplier_id=%s"""
+        rowsImpact = c.execute(query, [updatedVal, id, wh, suppID])
 
         if rowsImpact > 0:
-            return jsonify({'result': 'OK -- UPDATED', 'pn': id, 'warehouse':wh, 'supplier':suppID, 'newQty': currentQty}), 200
+            return jsonify({'result': 'OK -- UPDATED', 'pn': id, 'warehouse':wh, 'supplier':suppID, 'newQty': updatedVal}), 200
         else:
             return jsonify({'result': 'ERROR', 'msg': 'part number/supplierid/warehouse combo could not be increased'}), 400
     else:
         return jsonify({'result': 'ERROR', 'msg': 'part number/supplierid/warehouse combo does not exist'}), 400
 
-#TODO
-# fix error msg response, include HTTP status
+
+#Updates the quantity of a part number, supplier id in a warehouse to the specified amount
 @app.put('/updateQty/<string:id>/<int:wh>/<string:suppID>/<int:qty>')
 def update_qty(id,wh,suppID,qty):
-    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND warehouse_id=%d AND supplier_id=%s"""
+    query = """SELECT SUM(quantity) AS total FROM PARTS WHERE part_number =%s AND warehouse_id=%s AND supplier_id=%s"""
     c = db.cursor()
     rows = c.execute(query, [id, wh, suppID])
 
     if rows > 0:
-        query = """UPDATE TABLE PARTS SET quantity=%d WHERE part_number=%s AND warehouse_id=%d AND supplier_id=%s"""
+        query = """UPDATE PARTS SET quantity=%s WHERE part_number=%s AND warehouse_id=%s AND supplier_id=%s"""
         rowsImpact = c.execute(query, [qty, id, wh, suppID])
 
         if rowsImpact > 0:
@@ -175,8 +172,8 @@ def update_qty(id,wh,suppID,qty):
     else:
         return jsonify({'result': 'ERROR', 'msg': 'part number/supplierid/warehouse combo does not exist'}), 400
 
-#TODO
-# if pn+wh does not exist, error msg
+
+#Deletes a specific part number and supplier id combination from the database
 @app.delete('/deletePN/<string:id>/<string:suppID>')
 def remove_pn_wh(id,suppID):
     query = """SELECT part_number FROM PARTS WHERE part_number =%s AND supplier_id=%s"""
@@ -194,8 +191,8 @@ def remove_pn_wh(id,suppID):
     else:
         return jsonify({'result': 'ERROR', 'msg': 'part number/supplierid combo does not exist'}), 400
 
-#TODO
-# if pn does not exist, error msg
+
+#Deletes all instances of a part number from the database
 @app.delete('/deletePN/<string:id>')
 def remove_pn(id):
 
